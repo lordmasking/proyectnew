44d0523a8e16d3e745067764ca60f36fe2286d0e
Yorman Marenco <yorman.marenco@gmail.com>
2023-06-20 21:38:11 -0400

    commit

--- python/for.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-
-countdown = [4, 3, 2, 1, 0]
-
-for number in countdown:
-    print(number)
-    sleep(1)  # Wait 1 second
-print("Blast off!! 🚀")
--- sql/SQL.sql
+++
@@ -1,6 +0,0 @@
-SELECT OrderDate, COUNT(OrderID) AS Orders
-FROM Sales.SalesOrder
-WHERE Status = 'Shipped'
-GROUP BY OrderDate
-HAVING COUNT(OrderID) > 1
-ORDER BY OrderDate DESC;
